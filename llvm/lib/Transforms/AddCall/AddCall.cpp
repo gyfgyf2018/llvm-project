3be50df71f67161fde97d1b1bf7d2a104f3b31eb
Gao Yifan <945510703@qq.com>
2020-10-23 11:05:35 +0800

    2

---
+++ llvm/lib/Transforms/AddCall/AddCall.cpp
@@ -0,0 +1,20 @@
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+
+namespace {
+  struct AddCall : public ModulePass {
+    static char ID;
+    AddCall() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M) override {
+      errs() << "Hello: ";
+      errs().write_escaped(M.getName()) << '\n';
+      return false;
+    }
+  };
+}
+
+char AddCall::ID = 0;
+static RegisterPass<AddCall> X("addc", "Add an extern function call in main");
